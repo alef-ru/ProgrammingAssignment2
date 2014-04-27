31c408ba5647dc7086f6342a5ce56f8ab840231b
Alexander Efremov <a.s.efremov@gmail.com>
2014-04-27 13:31:54 +0400

    First version of programm

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,56 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This file contains functions for calculating inverse matrix using cache
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## This function creates a special "matrix" object that can cache its inverse
+makeCacheMatrix <- function(matrix) {
+  setmatrix <- function(m) {
+    if (!is.matrix(m)){
+      # User should be informed when he does something wrong
+      warning('It isn\'t martix!')
+      matrix <<-NULL
+    }else
+      matrix <<- m
+    inverse <<- NULL
+  }
+  getmatrix <- function() matrix
+  getinverse <- function() inverse
+  setinverse <- function(inv) inverse <<- inv
+  setmatrix(matrix)
+  list(set = setmatrix, get = getmatrix,
+       getinverse = getinverse,
+       setinverse = setinverse)  
 }
 
+## This function computes the inverse of the special "matrix" returned by makeCacheMatrix function.
+## If the inverse has already been calculated (and the matrix has not changed), 
+## then the cachesolve should retrieve the inverse from the cache.
+cacheSolve <- function(cacheMatrix) {
+  ## Return a matrix that is the inverse of 'x'
+  inv <- cacheMatrix$getinverse()
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  data <- cacheMatrix$get()
+  if(is.null(data)) {
+    warning("Matrix isn't set")
+    return(NULL)
+  }
+  inv <- computeInverse(data)
+  cacheMatrix$setinverse(inv)
+  inv
+}
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## This function calculates inverse for matix m
+computeInverse <- function(m){
+  if (!is.matrix(m)){
+    # User should be informed when he does something wrong
+    warning('It isn\'t martix!')
+    return(NULL)
+  }
+  if (dim(m)[1] == dim(m)[2])
+    return(solve(m))  
+  else{
+    warning('Sorry, but i can work only with square matrix :( ')
+    return(NULL)
+  } 
 }
